36a5a817205f33ca8916b00ccee779348ed55269
Saeed Jalali (Desktop) <jalalisaeid30@gmail.com>
2018-07-28 16:50:04 +0430

    Make first layout desgin

--- app/src/main/java/com/musicay/MainActivity.java
+++ app/src/main/java/com/musicay/MainActivity.java
@@ -2,6 +2,7 @@ package com.musicay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void btnClick(View view) {
+
+
+
+    }
+
 }
