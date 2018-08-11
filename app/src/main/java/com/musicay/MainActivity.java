bc48d3bab69f26622ce367c4b14c5ab32a3d89cc
Saeed Jalali (Desktop) <jalalisaeid30@gmail.com>
2018-08-11 11:15:05 +0430

    Adding imgview & imgbtn

--- app/src/main/java/com/musicay/MainActivity.java
+++ app/src/main/java/com/musicay/MainActivity.java
@@ -2,29 +2,26 @@ package com.musicay;
 
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import com.github.ybq.android.spinkit.style.CubeGrid;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn;
-    private TextView textView;
     private MyMusicay myMusicay;
+    private ImageButton imageBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = findViewById(R.id.btn);
-        textView = findViewById(R.id.textView);
 
+        ImageView imageView = findViewById(R.id.imageView);
+        imageBtn = findViewById(R.id.imageBtn);
         ProgressBar progressBar = (ProgressBar) findViewById(R.id.spin_kit);
         CubeGrid cubeGrid = new CubeGrid();
         progressBar.setIndeterminateDrawable(cubeGrid);
@@ -32,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         String musicURL = "http://host2.rjavan.stream/media/mp3/mp3-256/Mahbod-Tangna.mp3";
         myMusicay = new MyMusicay();
-        myMusicay.init(musicURL, progressBar, btn);
+        myMusicay.init(musicURL, progressBar, imageBtn, imageView);
     }
 
     @Override
@@ -42,32 +39,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnClick(View view) {
-        if (myMusicay.playOrPause()) {
-            btn.setText(R.string.resume);
-            Snackbar.make(view, "Music is Paused , Exit?", Snackbar.LENGTH_LONG)
-                    .setAction("Yes", new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            finish();
-                        }
-                    })
-                    .show();
-        } else {
-            btn.setText(R.string.pause);
-            Snackbar.make(view, "Music is Played.", Snackbar.LENGTH_LONG)
-                    .setAction(":)", new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            textView.setVisibility(View.VISIBLE);
-                            new Handler().postDelayed(new Runnable() {
-                                @Override
-                                public void run() {
-                                    textView.setVisibility(View.INVISIBLE);
-                                }
-                            }, 3000L);
-                        }
-                    })
-                    .show();
-        }
+        if (myMusicay.playOrPause())
+            imageBtn.setImageResource(R.drawable.ic_action_play);
+        else
+            imageBtn.setImageResource(R.drawable.ic_action_pause);
     }
 }
