ea0eb408457e51480707a0e82c6ac5f4d7100512
Saeed Jalali (Desktop) <jalalisaeid30@gmail.com>
2018-08-29 10:04:18 +0430

    Clean code

--- app/src/main/java/com/musicay/MainActivity.java
+++ app/src/main/java/com/musicay/MainActivity.java
@@ -7,28 +7,41 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
 
 import com.github.ybq.android.spinkit.style.ThreeBounce;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     private MyMusicay myMusicay;
     private ImageButton imageBtn;
+    private ImageView imageView;
+    private ProgressBar progressBar;
+    private String musicURL;
+    private ThreeBounce threeBounce;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUI();
+    }
 
-        ImageView imageView = findViewById(R.id.imageView);
+    private void initUI() {
+        imageView = findViewById(R.id.imageView);
         imageBtn = findViewById(R.id.imageBtn);
-        ProgressBar progressBar = (ProgressBar) findViewById(R.id.spin_kit);
-        ThreeBounce threeBounce = new ThreeBounce();
+        progressBar = findViewById(R.id.spin_kit);
+        musicURL = "http://host2.rjavan.stream/media/mp3/mp3-256/Amorphica-OxyLove-(Ft-Sepehr-Khalse-Pedi-I-Liona).mp3";
+
+        threeBounce = new ThreeBounce();
+        myMusicay = new MyMusicay();
+
         progressBar.setIndeterminateDrawable(threeBounce);
         progressBar.setVisibility(View.INVISIBLE);
 
-        String musicURL = "http://host2.rjavan.stream/media/mp3/mp3-256/Amorphica-OxyLove-(Ft-Sepehr-Khalse-Pedi-I-Liona).mp3";
-        myMusicay = new MyMusicay();
         myMusicay.init(musicURL, progressBar, imageBtn, imageView);
     }
 
@@ -38,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
-    public void btnClick(View view) {
+    public void playPause(View view) {
         if (myMusicay.playOrPause())
             imageBtn.setImageResource(R.drawable.ic_action_play);
         else
